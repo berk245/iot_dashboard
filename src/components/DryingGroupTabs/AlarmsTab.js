5b7c1513b6644967067b26991db5fd6abea54d70
berk <berk@mittagsbonus.de>
2021-10-28 19:14:51 +0200

    Create an alarms table component

--- src/components/DryingGroupTabs/AlarmsTab.js
+++ src/components/DryingGroupTabs/AlarmsTab.js
@@ -1,17 +1,7 @@
-import { makeStyles } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
+import AlarmsTable from '../Tables/AlarmsTable'
 
-const useStyles = makeStyles(() => ({
-  sensorTable: {
-    width: "95%",
-    margin: "2rem auto",
-    outline: "1px solid silver",
-    borderRadius: "5px",
-    padding: "1rem",
-    fontSize: "0.9rem",
-    lineHeight: "2rem",
-  },
-}));
+
 
 function AlarmsTab({
   dryingGroup,
@@ -23,8 +13,6 @@ function AlarmsTab({
   operatorNameAndIdPairs,
 }) {
   const [alarms, setAlarms] = useState([]);
-
-  const classes = useStyles();
 
   useEffect(() => {
     const getAlarms = async () => {
@@ -40,30 +28,7 @@ function AlarmsTab({
   return (
     <div>
       {alarms && (
-        <table className={classes.sensorTable}>
-          <tr >
-            <th style={{borderBottom:'2px solid silver'}}>Sensor</th>
-            <th style={{borderBottom:'2px solid silver'}}>Measurement Type</th>
-            <th style={{borderBottom:'2px solid silver'}}>Min Threshold</th>
-            <th style={{borderBottom:'2px solid silver'}}>Max Threshold</th>
-            <th style={{borderBottom:'2px solid silver'}}>Measurement Unit</th>
-          </tr>
-          {alarms.map((alarm, index) => {
-            return (
-              <>
-                <tr key={index}>
-                  <td style={{borderBottom:'1px solid silver', padding: '0.25rem'}}>
-                    {sensorNameAndIdPairs[alarm.sensor_id]} / {alarm.sensor_id}
-                  </td>
-                  <td style={{borderBottom:'1px solid silver', padding: '0.25rem'}}>{typeNameAndIdPairs[alarm.measurement_type_id]}</td>
-                  <td style={{borderBottom:'1px solid silver', padding: '0.25rem'}}>{alarm.min_allowed_threshold}</td>
-                  <td style={{borderBottom:'1px solid silver', padding: '0.25rem'}}>{alarm.max_allowed_threshold}</td>
-                  <td style={{borderBottom:'1px solid silver', padding: '0.25rem'}}>{unitNameAndIdPairs[alarm.measurement_type_id]}</td>
-                </tr>
-              </>
-            );
-          })}
-        </table>
+        <AlarmsTable alarms={alarms} sensorNameAndIdPairs={sensorNameAndIdPairs} typeNameAndIdPairs={typeNameAndIdPairs} unitNameAndIdPairs={unitNameAndIdPairs} /> 
       )}
     </div>
   );
